987cab8600935f1b6a3d0c2b506c7bf6d5f1d202
Dhanushka <dhanushkamcr@gmail.com>
2025-11-22 14:49:48 -0600

    Implemented Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,27 @@
-const Button =() => {
+import React from "react";
+
+const varientClasses = {
+    primary: 'bg-blue-600 text-white',
+    secondary: 'bg-gray-700 text-gray-800',
+    danger: 'bg-red-600 text-white',
+}
+
+const Button =({
+    variant='primary',
+    children,
+    className = '',
+    onclick,
+    ...props
+}) => {
     return (
-        <div></div>
+        <div>
+            <button className={`px-5 py-2 m-1 rounded-3xl font-semibold transition-colors
+                ${varientClasses[variant] ?? varientClasses.primary} ${className}`}
+                onClick={onclick} {...props}
+            >
+                {children}
+            </button>
+        </div>
     )
 }
 
