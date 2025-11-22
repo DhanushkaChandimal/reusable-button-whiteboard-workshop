27d521db8654515f81a9415e93870ec9defa0186
Dhanushka <dhanushkamcr@gmail.com>
2025-11-22 15:21:42 -0600

    Handle function in onClick

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 
 const Modal =({
@@ -6,14 +6,29 @@ const Modal =({
     closeModal
 }) => {
 
+    const [activate, setActivate] = useState(false)
+
     if(!isOpen) return null;
+
+    function handleButton() {
+        setActivate(true)
+    }
+
 
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
             <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-md relative">
                 <div>Modal Content</div>
+
+                {activate && (
+                    <h2 className='text-3xl font-extrabold bg-gradient-to-r from-pink-500 via-yellow-400 to-blue-500
+                        text-transparent bg-clip-text drop-shadow-lg p-2 mb-2 animate-pulse'
+                    >Function Running!
+                    </h2>
+                )}
                
                 <Button onClick={()=>closeModal(false)}>Close Modal</Button>
+                <Button onClick={handleButton}>Run Function</Button>
             </div>
         </div>
     )
